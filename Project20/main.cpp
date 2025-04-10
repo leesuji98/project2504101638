343792726c2e6bbdb44e280213733924b8814b61
Jonghwi <ellia2431@naver.com>
2025-04-10 17:11:45 +0900

    add getSum()

--- Project20/main.cpp
+++ Project20/main.cpp
@@ -2,11 +2,18 @@
 
 class Cal {
 public:
-
+	int getSum(int a, int b) {
+		return a + b;
+	}
 };
 
 TEST(t1, t2) {
 	EXPECT_EQ(1, 1);
+}
+
+TEST(t1, sumTest) {
+	Cal cal;
+	EXPECT_EQ(3, cal.getSum(1, 2));
 }
 
 int main() {
