b906deb1a2afa2346964235956b824cc00703f12
kdh <dongri.kang@sansumg.com>
2025-04-10 17:13:00 +0900

    add getGop()

--- Project20/main.cpp
+++ Project20/main.cpp
@@ -2,11 +2,21 @@
 
 class Cal {
 public:
-
+	int getGop(int a, int b)
+	{
+		return a * b;
+	}
 };
 
 TEST(t1, t2) {
 	EXPECT_EQ(1, 1);
+}
+
+TEST(t1, GopTest)
+{
+	Cal stCal;
+
+	EXPECT_EQ(10, stCal.getGop(2, 5));
 }
 
 int main() {
