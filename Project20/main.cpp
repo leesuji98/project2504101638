36830db4c1b3e899068718943ae0ed5e746e6537
leesuji <leesuji98@naver.com>
2025-04-10 17:12:42 +0900

    Divide

--- Project20/main.cpp
+++ Project20/main.cpp
@@ -2,11 +2,27 @@
 
 class Cal {
 public:
-
+	Cal() {};
+	int getDivide(int a, int b) {
+		if (b == 0)
+			return -1;
+		return a / b;
+	}
 };
 
-TEST(t1, t2) {
-	EXPECT_EQ(1, 1);
+TEST(t1, Divide1) {
+	Cal c1;
+	EXPECT_EQ(-1, c1.getDivide(1, 0));
+}
+
+TEST(t1, Divide2) {
+	Cal c1;
+	EXPECT_EQ(5, c1.getDivide(10, 2));
+}
+
+TEST(t1, Divide3) {
+	Cal c1;
+	EXPECT_EQ(3, c1.getDivide(9, 3));
 }
 
 int main() {
