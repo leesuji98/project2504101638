72ad9c7a2b43ede6f72de1c81cc8c087854ecb70
wanny <wankims@gmail.com>
2025-04-10 17:23:04 +0900

    add zegop logic and test code

--- Project20/main.cpp
+++ Project20/main.cpp
@@ -2,11 +2,19 @@
 
 class Cal {
 public:
-
+	int getZegop(int a) {
+		return (a * a);
+	}
 };
 
 TEST(t1, t2) {
 	EXPECT_EQ(1, 1);
+}
+
+TEST(t1, zegop) {
+	Cal cal;
+	int ret = cal.getZegop(10);
+	EXPECT_EQ(100, ret);
 }
 
 int main() {
