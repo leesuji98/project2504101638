f3ec7abe6c6f62b3903192c61a6e41318c1c8f0e
Hyeonseung Yu <hyeonseung.yu@gmail.com>
2025-04-10 17:12:22 +0900

    getMinus func added.

--- Project20/main.cpp
+++ Project20/main.cpp
@@ -2,11 +2,20 @@
 
 class Cal {
 public:
-
+	int getMinus(int a, int b) {
+		return a - b;
+	}
 };
 
 TEST(t1, t2) {
 	EXPECT_EQ(1, 1);
+}
+
+TEST(t1, GetMinusTest) {
+	Cal c;
+	int expected = 4;
+	int actual = c.getMinus(10, 6);
+	EXPECT_EQ(expected, actual);
 }
 
 int main() {
